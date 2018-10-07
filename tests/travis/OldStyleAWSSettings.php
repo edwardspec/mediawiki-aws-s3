6fa6561bc772e1a47a0df0bb551cc9817faee119
Edward Chernenko <edwardspec@gmail.com>
2018-10-07 11:38:50 +0300

    (doc) Update OldStyleAWSSettings.php (example config) to use 1 bucket
    This file is used as an example of "how to configure extension with
    customized $wgLocalFileRepo/$wgFileBackends".
    
    Use of four S3 buckets is deprecated and shouldn't be in the example.

--- tests/travis/OldStyleAWSSettings.php
+++ tests/travis/OldStyleAWSSettings.php
@@ -16,16 +16,16 @@
 
 /**
 	@file
-	@brief Old-style version of [AWSSettings.php]. Doesn't use $wgAWSBucketPrefix.
+	@brief Old-style version of [AWSSettings.php]. Doesn't use $wgAWSBucketName.
 */
 
 $KEY = getenv( 'AWS_KEY' );
 $SECRET = getenv( 'AWS_SECRET' );
-$BUCKET_PREFIX = getenv( 'AWS_BUCKET_PREFIX' );
+$BUCKET = getenv( 'BUCKET' );
 
 /*---------------------------------------------------------------------------*/
 
-require_once "$IP/extensions/AWS/AWS.php";
+wfLoadExtension( 'AWS' );
 
 $wgAWSCredentials['key'] = $KEY;
 $wgAWSCredentials['secret'] = $SECRET;
@@ -33,10 +33,10 @@ $wgAWSCredentials['secret'] = $SECRET;
 $wgAWSRegion = 'us-east-1'; # Northern Virginia
 
 $wgFileBackends['s3']['containerPaths'] = [
-	"$wgDBname-local-public" => "${BUCKET_PREFIX}",
-	"$wgDBname-local-thumb" => "${BUCKET_PREFIX}-thumb",
-	"$wgDBname-local-deleted" => "${BUCKET_PREFIX}-deleted",
-	"$wgDBname-local-temp" => "${BUCKET_PREFIX}-temp"
+	"$wgDBname-local-public" => "${BUCKET}",
+	"$wgDBname-local-thumb" => "${BUCKET}/thumb",
+	"$wgDBname-local-deleted" => "${BUCKET}/deleted",
+	"$wgDBname-local-temp" => "${BUCKET}/temp"
 ];
 
 $wgLocalFileRepo = [
@@ -46,8 +46,8 @@ $wgLocalFileRepo = [
 	'url'               => $wgScriptPath . '/img_auth.php',
 	'hashLevels'        => 0,
 	'zones'             => [
-		'public'  => [ 'url' => "https://${BUCKET_PREFIX}.s3.amazonaws.com" ],
-		'thumb'   => [ 'url' => "https://${BUCKET_PREFIX}-thumb.s3.amazonaws.com" ],
+		'public'  => [ 'url' => "https://${BUCKET}.s3.amazonaws.com" ],
+		'thumb'   => [ 'url' => "https://${BUCKET}.s3.amazonaws.com/thumb" ],
 		'temp'    => [ 'url' => false ],
 		'deleted' => [ 'url' => false ]
 	]
